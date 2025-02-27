61d7114ea986e24993c521f6e54bb4a3116b8e59
RobertZaharie <79197580+RobertZaharie@users.noreply.github.com>
2025-02-27 15:25:01 +0200

    Update lab1.cpp
    pwps

--- lab1.cpp
+++ lab1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Masina {
 protected:
 	list<string> pasageri;
-    string driver;
+	string driver;
 	string nrInmatriculare;
 	int nrLocuri;
 	string marca;
@@ -15,9 +15,27 @@ protected:
 
 public:
 
-    Masina(string driver, string nrInmatriculare, int nrLocuri, string marca, string combustibil) : driver(driver), nrInmatriculare(nrInmatriculare), nrLocuri(nrLocuri), marca(marca), combustibil(combustibil)  {}
+	Masina(string driver, string nrInmatriculare, int nrLocuri, string marca, string combustibil) : driver(driver), nrInmatriculare(nrInmatriculare), nrLocuri(nrLocuri), marca(marca), combustibil(combustibil) {}
+	void SetPassengers(string NumePasager) {
+		this->pasageri.push_back(NumePasager);
+	}
 
-    
+	void detaliiMasina() {
+		cout <<"Nr de locuri " << this->nrLocuri<<endl;
+		cout << "Conducator " << this->driver<<endl;
+		cout << "nr matriculare " << this->nrInmatriculare<<endl;
+	
+	}
+
+	void Pasageri()
+	{
+
+		cout << " nume Pasageri: " << endl;
+		for (string nume : this->pasageri)
+			cout << nume << endl;
+	}
+
+		 
 };
 
 class Electric : public Masina {
@@ -25,9 +43,9 @@ private:
 
 public:
 
-    Electric(string driver, string nrInmatriculare, int nrLocuri, string marca, string combustibil) : Masina(pasageri, driver, nrInmatriculare, nrLocuri, marca, "Electric") {}
+	Electric(string driver, string nrInmatriculare, int nrLocuri, string marca) : Masina( driver, nrInmatriculare, nrLocuri, marca, "Electric") {}
 
-    
+
 };
 
 class IntComb : public Masina {
@@ -35,9 +53,9 @@ private:
 
 public:
 
-	IntComb(string driver, string nrInmatriculare, int nrLocuri, string marca, string combustibil) : Masina(pasageri, driver, nrInmatriculare, nrLocuri, marca, "Diesel") {}
+	IntComb(string driver, string nrInmatriculare, int nrLocuri, string marca) : Masina( driver, nrInmatriculare, nrLocuri, marca, "Diesel") {}
 
-    
+
 };
 
 
@@ -45,5 +63,18 @@ public:
 
 int main()
 {
+	Electric masina1((string)"Marcel", (string)"tbc", 4, (string)"honda");
+	Electric masina2((string)"Dorel", (string)"thc", 4, (string)"Dacia");
+	IntComb masina3((string)"Nenorocel", (string)"sod", 4, (string)"honda");
+	IntComb masina4((string)"Clopotel", (string)"ask", 4, (string)"Logan");
+
+	masina2.SetPassengers("Ioana");
+	masina2.SetPassengers("Robert");
+	masina2.SetPassengers("Alex");
+
+	masina2.detaliiMasina();
+	masina2.Pasageri();
+
+
 	return 0;
 }
